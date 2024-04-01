99bf926ff71ce8a002000ae74c1fad73568c6f1b
Fajrii <162245842+Bintara67@users.noreply.github.com>
2024-04-01 20:52:47 +0800

    Add files via upload

---
+++ get_oop/oop_get.php
@@ -0,0 +1,20 @@
+<?php
+// Include file karyawan.php dan keuangan.php
+include 'karyawan(get)_oop.php';
+include 'keuangan(get)_oop.php';
+
+// Membuat objek karyawan
+$karyawan1 = new karyawan("Fajar", "Manager", 10000000);
+$karyawan2 = new karyawan("Bintara", "Staff", 5000000);
+
+// Menampilkan informasi karyawan
+echo "<h2>Informasi Karyawan</h2>";
+$karyawan1->displayInfo();
+$karyawan2->displayInfo();
+
+// Menghitung total gaji karyawan
+$karyawan = array($karyawan1, $karyawan2);
+$totalGaji = FinancialManagement::hitungTotalGaji($karyawan);
+
+echo "<h2>Total Pengeluaran Gaji Karyawan</h2>";
+echo "Total: Rp " . number_format($totalGaji);
